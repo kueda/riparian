861fd869850aefb9e8230a1db5a2ed857d184123
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-07-06 16:26:33 -0700

    Commas are important.

--- lib/app/models/flow_task_resource.rb
+++ lib/app/models/flow_task_resource.rb
@@ -2,6 +2,6 @@ class FlowTaskResource < ActiveRecord::Base
   belongs_to :flow_task
   belongs_to :resource, :polymorphic => true
   has_attached_file :file,
-    :path => Riparian.config.flow_task_resource_file_path
+    :path => Riparian.config.flow_task_resource_file_path,
     :url => Riparian.config.flow_task_resource_file_url
 end
