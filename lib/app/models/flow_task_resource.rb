99d6a37d469533746842195aff89ec7de0eb641a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-08-02 14:12:39 -0700

    Made sure attachments aren't empty.

--- lib/app/models/flow_task_resource.rb
+++ lib/app/models/flow_task_resource.rb
@@ -4,4 +4,5 @@ class FlowTaskResource < ActiveRecord::Base
   has_attached_file :file,
     :path => ":rails_root/public/system/:class/:id/:filename",
     :url => "/system/:class/:id/:filename"
+  validates_attachment_size :file, :greater_than => 1.byte
 end
